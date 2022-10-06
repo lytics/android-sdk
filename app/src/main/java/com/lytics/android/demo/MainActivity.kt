1a9c9693be8b5295d756113a1b87b758f03b0b95
Jason Emerick <jemerick@gmail.com>
2022-10-06 16:34:44 -0400

    update demo to just display current user info

--- app/src/main/java/com/lytics/android/demo/MainActivity.kt
+++ app/src/main/java/com/lytics/android/demo/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.lytics.android.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import com.google.android.gms.ads.identifier.AdvertisingIdClient
-import kotlinx.coroutines.*
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.lytics.android.Lytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Lytics Demo"
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.setText("${Lytics.currentUser}")
     }
 }
