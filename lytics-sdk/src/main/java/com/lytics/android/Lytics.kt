e335fe1899921388dd8d3fdbf42e94f159dbaa46
Jason Emerick <jemerick@gmail.com>
2022-10-12 23:22:44 -0400

    make context, config, and prefs vars lateinit

--- lytics-sdk/src/main/java/com/lytics/android/Lytics.kt
+++ lytics-sdk/src/main/java/com/lytics/android/Lytics.kt
@@ -40,6 +40,11 @@ object Lytics {
     private var isInitialized: Boolean = false
 
     /**
+     * Returns true if this singleton instance has been initialized
+     */
+    private var isInitialized: Boolean = false
+
+    /**
      * The current Lytics user
      */
     var currentUser: LyticsUser? = null
