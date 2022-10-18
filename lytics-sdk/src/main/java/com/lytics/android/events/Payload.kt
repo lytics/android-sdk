9d30b18e5106b37436640376fd2c7f97856f783b
Jason Emerick <jemerick@gmail.com>
2022-10-18 18:33:26 -0400

    Update payload to serialize and deserialize from database

--- lytics-sdk/src/main/java/com/lytics/android/events/Payload.kt
+++ lytics-sdk/src/main/java/com/lytics/android/events/Payload.kt
@@ -2,21 +2,71 @@ package com.lytics.android.events
 
 import com.lytics.android.Constants
 import com.lytics.android.JsonSerializable
+import com.lytics.android.Lytics
+import com.lytics.android.toMap
 import org.json.JSONObject
 
 internal data class Payload(
+    var id: Long? = null,
     var stream: String,
-    var eventName: String? = null,
+    var data: Map<String, Any?>? = null,
     var identifiers: Map<String, Any?>? = null,
     var attributes: Map<String, Any?>? = null,
     var properties: Map<String, Any?>? = null,
     var consent: Map<String, Any?>? = null,
 ) : JsonSerializable {
+
+    constructor(id: Long, stream: String, payload: String) : this(id, stream) {
+        val jsonObject = JSONObject(payload)
+        val jsonData = jsonObject.toMap()
+        identifiers = jsonData[Constants.KEY_IDENTIFIERS] as? Map<String, Any?>
+        attributes = jsonData[Constants.KEY_ATTRIBUTES] as? Map<String, Any?>
+        properties = jsonData[Constants.KEY_PROPERTIES] as? Map<String, Any?>
+        consent = jsonData[Constants.KEY_CONSENT] as? Map<String, Any?>
+        data = jsonData.minus(
+            listOf(
+                Constants.KEY_IDENTIFIERS,
+                Constants.KEY_ATTRIBUTES,
+                Constants.KEY_PROPERTIES,
+                Constants.KEY_CONSENT
+            )
+        )
+    }
+
+    constructor(event: LyticsIdentityEvent) : this(
+        stream = event.stream ?: Lytics.configuration.defaultStream,
+        identifiers = event.identifiers,
+        attributes = event.attributes,
+    ) {
+        event.name?.let {
+            data = mapOf(Constants.KEY_EVENT_NAME to it)
+        }
+    }
+
+    constructor(event: LyticsConsentEvent) : this(
+        stream = event.stream ?: Lytics.configuration.defaultStream,
+        identifiers = event.identifiers,
+        attributes = event.attributes,
+        consent = event.consent,
+    ) {
+        event.name?.let {
+            data = mapOf(Constants.KEY_EVENT_NAME to it)
+        }
+    }
+
+    constructor(event: LyticsEvent) : this(
+        stream = event.stream ?: Lytics.configuration.defaultStream,
+        identifiers = event.identifiers,
+        properties = event.properties,
+    ) {
+        event.name?.let {
+            data = mapOf(Constants.KEY_EVENT_NAME to it)
+        }
+    }
+
     override fun serialize(): JSONObject {
-        return JSONObject().apply {
-            eventName?.let {
-                put(Constants.KEY_EVENT_NAME, it)
-            }
+        val json = data?.let { JSONObject(it) } ?: JSONObject()
+        json.apply {
             identifiers?.let {
                 put(Constants.KEY_IDENTIFIERS, JSONObject(it))
             }
@@ -30,7 +80,6 @@ internal data class Payload(
                 put(Constants.KEY_CONSENT, JSONObject(it))
             }
         }
+        return json
     }
-
-
 }
