a0726980b3741cc35c5cecc8ebc0ad413003d91b
Jason Emerick <jemerick@gmail.com>
2022-10-18 18:34:47 -0400

    add configuration for max retry count

--- lytics-sdk/src/main/java/com/lytics/android/LyticsConfiguration.kt
+++ lytics-sdk/src/main/java/com/lytics/android/LyticsConfiguration.kt
@@ -41,6 +41,11 @@ data class LyticsConfiguration(
     val maxQueueSize: Int = 10,
 
     /**
+     * The max number of times to try and resend an event on failure
+     */
+    val maxRetryCount: Int = 3,
+
+    /**
      * The interval in milliseconds at which the event queue is uploaded to the Lytics API. Set to 0 to disable.
      * Defaults to 1 second.
      */
