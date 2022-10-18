b85613a5ace7b22c9d463d25f5ec8b670399e0d0
Jason Emerick <jemerick@gmail.com>
2022-10-18 18:54:24 -0400

    add collection endpoint setting

--- lytics-sdk/src/main/java/com/lytics/android/LyticsConfiguration.kt
+++ lytics-sdk/src/main/java/com/lytics/android/LyticsConfiguration.kt
@@ -69,4 +69,16 @@ data class LyticsConfiguration(
      * Default is disabled.
      */
     val sandboxMode: Boolean = false,
-)
+
+    /**
+     * The bulk endpoint URLs to send events. The stream name will be appended to the endpoint URLs.
+     *
+     * As an example, for the default endpoint of `https://api.lytics.io/collect/json/` events for the stream `crm` will
+     * be sent to `https://api.lytics.io/collect/json/crm`
+     */
+    val collectionEndpoint: String = DEFAULT_COLLECTION_ENDPOINT,
+) {
+    companion object {
+        val DEFAULT_COLLECTION_ENDPOINT = "https://api.lytics.io/collect/json/"
+    }
+}
