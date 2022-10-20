6d2daee2b113c426ee03a6efb1e7cf326396c7e2
Jason Emerick <jemerick@gmail.com>
2022-10-19 23:09:39 -0400

    add util to check network connectivity

--- lytics-sdk/src/main/java/com/lytics/android/Utils.kt
+++ lytics-sdk/src/main/java/com/lytics/android/Utils.kt
@@ -1,8 +1,10 @@
 package com.lytics.android
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import android.os.Process
 import java.util.*
 
@@ -25,5 +27,37 @@ internal object Utils {
             Process.myPid(),
             Process.myUid()
         ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    /**
+     * Get the current, active networks connection status.  If we cannot access network state due to lacking permissions
+     * or no connectivity manager, return null indicating unknown connection status. Otherwise get the active network
+     * and return it's connection status.
+     *
+     * @return true if connected, false if not connected, null if unknown or cannot check
+     */
+    @Suppress("DEPRECATION")
+    @SuppressLint("MissingPermission")
+    fun getConnectionStatus(context: Context): Boolean? {
+        // check if we can event check network connection status
+        if (!hasPermission(context, Manifest.permission.ACCESS_NETWORK_STATE)) {
+            Lytics.logger.debug("No ACCESS_NETWORK_STATE permission to check network status.")
+            return null
+        }
+        val connectivityManager: ConnectivityManager? =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+        if (connectivityManager == null) {
+            Lytics.logger.debug("ConnectivityManager is null. Cannot check network status.");
+            return null
+        }
+
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        if (activeNetworkInfo == null) {
+            Lytics.logger.debug("No active network, no connection")
+            return false
+        }
+        val isConnected = activeNetworkInfo.isConnected
+        Lytics.logger.debug("Active network is connected: $isConnected")
+        return isConnected
     }
 }
