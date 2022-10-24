345180a6018e5903b8a9e2a9556ea1bcb3cd5da0
Jason Emerick <jemerick@gmail.com>
2022-10-24 18:34:26 -0400

    update register view to send events

--- app/src/main/java/com/lytics/android/demo/ui/account/RegisterFragment.kt
+++ app/src/main/java/com/lytics/android/demo/ui/account/RegisterFragment.kt
@@ -1,12 +1,20 @@
 package com.lytics.android.demo.ui.account
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.lytics.android.Lytics
+import com.lytics.android.demo.R
 import com.lytics.android.demo.databinding.FragmentRegisterBinding
+import com.lytics.android.events.LyticsConsentEvent
+import com.lytics.android.events.LyticsIdentityEvent
 
 class RegisterFragment : Fragment() {
 
@@ -20,6 +28,60 @@ class RegisterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+
+        Lytics.currentUser?.let {
+            val name: String = it.attributes?.get("name") as? String ?: ""
+            binding.nameTextField.editText?.setText(name)
+            val email: String = it.identifiers?.get("email") as? String ?: ""
+            binding.emailTextField.editText?.setText(email)
+
+            val tac: Boolean = it.consent?.get("tac") as? Boolean ?: false
+            binding.privacyPolicyCheckbox.isChecked = tac
+            val idfa: Boolean = it.consent?.get("idfa") as? Boolean ?: false
+            binding.idfaCheckbox.isChecked = idfa
+        }
+
+        binding.registerButton.setOnClickListener {
+            val email = binding.emailTextField.editText?.text.toString()
+            val name = binding.nameTextField.editText?.text.toString()
+            Lytics.identify(
+                LyticsIdentityEvent(
+                    name = "login",
+                    identifiers = mapOf("email" to email),
+                    attributes = mapOf("name" to name)
+                )
+            )
+
+            Toast.makeText(requireContext(), R.string.signed_up, Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.navigation_events)
+        }
+
+        binding.privacyPolicyCheckbox.setOnClickListener {
+            val termsAndConditions = binding.privacyPolicyCheckbox.isChecked
+            Lytics.consent(LyticsConsentEvent(name = "android consent", consent = mapOf("tac" to termsAndConditions)))
+        }
+
+        binding.idfaCheckbox.setOnClickListener {
+            val idfa = binding.idfaCheckbox.isChecked
+            if (idfa) {
+                val builder = AlertDialog.Builder(requireContext())
+                builder.apply {
+                    setMessage(R.string.idfa_prompt)
+                    setPositiveButton(R.string.allow_tracking) { _, _ ->
+                        Lytics.enableIDFA()
+                        Lytics.consent(LyticsConsentEvent(name = "android consent", consent = mapOf("idfa" to idfa)))
+                    }
+                    setNegativeButton(R.string.do_no_track) { _, _ ->
+                        binding.idfaCheckbox.isChecked = false
+                    }
+                }
+                builder.create().show()
+            } else {
+                Lytics.disableIDFA()
+                Lytics.consent(LyticsConsentEvent(name = "android consent", consent = mapOf("idfa" to idfa)))
+            }
+        }
+
         return binding.root
     }
 
