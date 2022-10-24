2395a8a682226a2a543549d3b7d7f47c91c1eba4
Jason Emerick <jemerick@gmail.com>
2022-10-24 18:19:10 -0400

    remove app demo lytics API calls

--- app/src/main/java/com/lytics/android/demo/DemoApplication.kt
+++ app/src/main/java/com/lytics/android/demo/DemoApplication.kt
@@ -16,39 +16,5 @@ class DemoApplication : Application() {
         Lytics.init(applicationContext, config)
 
         Lytics.optIn()
-
-        val idEvent = LyticsIdentityEvent().apply {
-            stream = "androidIdentify"
-            identifiers = mapOf("userId" to "this-users-known-id-or-something", "email" to "someemail@lytics.com")
-            attributes = mapOf("firstName" to "Mark", "lastName" to "Hayden", "title" to "VP Product")
-        }
-        Lytics.identify(idEvent)
-
-        val consentEvent = LyticsConsentEvent().apply {
-            stream = "androidConsent"
-            identifiers = mapOf("userId" to "this-users-known-id-or-something", "email" to "someemail@lytics.com")
-            attributes = mapOf("firstName" to "Mark", "lastName" to "Hayden", "title" to "VP Product")
-            consent = mapOf(
-                "document" to "gdpr_collection_agreement_v1",
-                "timestamp" to "46236424246",
-                "consented" to "true",
-            )
-        }
-        Lytics.consent(consentEvent)
-
-        val event = LyticsEvent().apply {
-            stream = "ios"
-            name = "cart_add"
-            identifiers = mapOf("userId" to "this-users-known-id-or-something")
-            properties = mapOf("orderId" to "some-order-id", "total" to "19.95")
-        }
-        Lytics.track(event)
-
-        val screenEvent = LyticsEvent().copy(name="cart_view")
-        Lytics.screen(screenEvent)
-
-        Lytics.disableIDFA()
-
-        Lytics.dispatch()
     }
 }
