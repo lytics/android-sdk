5c2739cf467c366794f5d96e4c0689bb4743d46a
Jason Emerick <jemerick@gmail.com>
2022-10-25 18:51:27 -0400

    update demo config

--- app/src/main/java/com/lytics/android/demo/DemoApplication.kt
+++ app/src/main/java/com/lytics/android/demo/DemoApplication.kt
@@ -1,18 +1,27 @@
 package com.lytics.android.demo
 
 import android.app.Application
+import android.util.Log
 import com.lytics.android.LyticsConfiguration
 import com.lytics.android.Lytics
 import com.lytics.android.events.LyticsConsentEvent
 import com.lytics.android.events.LyticsEvent
 import com.lytics.android.events.LyticsIdentityEvent
 import com.lytics.android.logging.LogLevel
+import java.util.concurrent.TimeUnit
 
 class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val config = LyticsConfiguration(apiKey = "API KEY", defaultStream = "defaultStream", logLevel = LogLevel.DEBUG)
+        val config = LyticsConfiguration(
+            apiKey = "YOUR API KEY HERE",
+            defaultStream = "default",
+            logLevel = LogLevel.DEBUG,
+            autoTrackActivityScreens = true,
+            autoTrackAppOpens = true,
+            autoTrackFragmentScreens = true,
+        )
         Lytics.init(applicationContext, config)
 
         Lytics.optIn()
