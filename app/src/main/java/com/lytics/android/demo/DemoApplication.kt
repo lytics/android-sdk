c0a0bc4ad6ce1e36032f9b8cf1556d31781863ff
Jason Emerick <jemerick@gmail.com>
2022-11-02 16:39:29 -0400

    update imports

--- app/src/main/java/com/lytics/android/demo/DemoApplication.kt
+++ app/src/main/java/com/lytics/android/demo/DemoApplication.kt
@@ -1,14 +1,9 @@
 package com.lytics.android.demo
 
 import android.app.Application
-import android.util.Log
-import com.lytics.android.LyticsConfiguration
 import com.lytics.android.Lytics
-import com.lytics.android.events.LyticsConsentEvent
-import com.lytics.android.events.LyticsEvent
-import com.lytics.android.events.LyticsIdentityEvent
+import com.lytics.android.LyticsConfiguration
 import com.lytics.android.logging.LogLevel
-import java.util.concurrent.TimeUnit
 
 class DemoApplication : Application() {
     override fun onCreate() {
