2f5574424f2a375d87fec26d497be65f1b5eccc7
Rae Eversmann <rae.eversmann@gmail.com>
2022-12-27 13:13:47 -0600

    added github actions

--- app/src/main/java/com/lytics/android/demo/DemoApplication.kt
+++ app/src/main/java/com/lytics/android/demo/DemoApplication.kt
@@ -10,11 +10,12 @@ class DemoApplication : Application() {
         super.onCreate()
 
         val config = LyticsConfiguration(
-            apiKey = "YOUR API KEY HERE",
+            apiKey = "at.d14ed80c766b500e5b4b1684b02038bd.7ccfb5bfcf0c2a3825776c0120e6de02",
             logLevel = LogLevel.DEBUG,
             autoTrackActivityScreens = true,
             autoTrackAppOpens = true,
             autoTrackFragmentScreens = true,
+            defaultStream = "mobile"
         )
         Lytics.init(applicationContext, config)
 
