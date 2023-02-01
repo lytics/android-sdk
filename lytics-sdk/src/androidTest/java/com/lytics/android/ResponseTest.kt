76316d2bbd9b7baaeb14363b19cf66d76c545780
Jason Emerick <jemerick@gmail.com>
2023-02-01 17:59:08 -0500

    add tests to response model tests

--- lytics-sdk/src/androidTest/java/com/lytics/android/ResponseTest.kt
+++ lytics-sdk/src/androidTest/java/com/lytics/android/ResponseTest.kt
@@ -11,7 +11,9 @@ class ResponseTest {
     fun testIsOk() {
         Assert.assertFalse(Response().isOk) // status code is 0
         Assert.assertTrue(Response(statusCode = 1).isOk)
+        Assert.assertEquals(Response(statusCode = 1).statusCode, 1)
         Assert.assertTrue(Response(statusCode = 200).isOk)
+        Assert.assertEquals(Response(statusCode = 200).statusCode, 200)
         Assert.assertTrue(Response(statusCode = 399).isOk)
         Assert.assertFalse(Response(statusCode = 400).isOk)
         Assert.assertFalse(Response(statusCode = 404).isOk)
@@ -22,6 +24,7 @@ class ResponseTest {
     fun testToJson() {
         Assert.assertNull(Response().json)  // data is null
         Assert.assertNull(Response(data = "not json").json)  // just a string
+        Assert.assertEquals(Response(data = "not json").data, "not json")
         val json = Response(data = "{\"test\": 1}").json
         Assert.assertNotNull(json)
         Assert.assertEquals(mapOf("test" to 1), json?.toMap())
