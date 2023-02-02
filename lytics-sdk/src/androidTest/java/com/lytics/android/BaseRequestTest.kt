b1a7966aeb92929e5b3b76ad7d18a23833b10ea9
Jason Emerick <jemerick@gmail.com>
2023-02-02 13:14:40 -0500

    add tests for base request

--- lytics-sdk/src/androidTest/java/com/lytics/android/BaseRequestTest.kt
+++ lytics-sdk/src/androidTest/java/com/lytics/android/BaseRequestTest.kt
@@ -1,8 +1,34 @@
 package com.lytics.android
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.concurrent.TimeUnit
 
+
+private class TestRequest(
+    private val url: String = "https://www.lytics.com/",
+    method: RequestMethod = RequestMethod.GET
+) : BaseRequest(method) {
+    override fun buildURL(): URL {
+        return URL(url)
+    }
+
+    override fun buildRequestData(): String? {
+        return when(requestMethod) {
+            RequestMethod.GET -> super.buildRequestData()
+            RequestMethod.POST -> "POST THIS"
+        }
+    }
+}
+
+@RunWith(AndroidJUnit4::class)
 class BaseRequestTest {
     @Test
     fun testRequestMethod() {
@@ -11,5 +37,157 @@ class BaseRequestTest {
 
         Assert.assertTrue(RequestMethod.POST.output)
         Assert.assertEquals("POST", RequestMethod.POST.method)
+    }
+
+    @Test
+    fun testBuildURL() {
+        val request = TestRequest()
+        Assert.assertEquals("https://www.lytics.com/", request.buildURL().toString())
+    }
+
+    @Test
+    fun testBadUrl() {
+        val request = TestRequest("")
+        val response = request.send()
+        Assert.assertEquals(0, response.statusCode)
+        Assert.assertEquals("java.net.MalformedURLException: no protocol: ", response.data)
+    }
+
+    @Test
+    fun testBuildHeaders() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val request = TestRequest()
+        Assert.assertEquals(
+            mapOf(
+                "Authorization" to "API_KEY",
+                "Connection" to "close",
+            ), request.buildHeaders()
+        )
+    }
+
+    @Test
+    fun testBuildRequestData() {
+        val request = TestRequest()
+        Assert.assertNull(request.buildRequestData())
+        Assert.assertEquals(RequestMethod.GET, request.requestMethod)
+    }
+
+    @Test
+    fun testSendSuccess() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setBody("hello, world!"))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.send()
+
+        Assert.assertEquals(200, response.statusCode)
+        Assert.assertEquals("hello, world!", response.data)
+
+        val recordedRequest = server.takeRequest()
+        Assert.assertEquals("GET", recordedRequest.method)
+        Assert.assertEquals("API_KEY", recordedRequest.headers["Authorization"])
+    }
+
+    @Test
+    fun testSendFail() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(500).setBody("server error!"))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.send()
+
+        Assert.assertEquals(500, response.statusCode)
+        Assert.assertEquals("server error!", response.data)
+
+        val recordedRequest = server.takeRequest()
+        Assert.assertEquals("GET", recordedRequest.method)
+        Assert.assertEquals("API_KEY", recordedRequest.headers["Authorization"])
+    }
+
+    @Test
+    fun testTimeout() {
+        Lytics.configuration = LyticsConfiguration(
+            "API_KEY",
+            networkRequestTimeout = TimeUnit.SECONDS.toMillis(1).toInt(),
+        )
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setHeadersDelay(2, TimeUnit.SECONDS))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.send()
+
+        Assert.assertEquals(0, response.statusCode)
+        Assert.assertEquals("java.net.SocketTimeoutException: timeout", response.data)
+    }
+
+    @Test
+    fun testSendWithRetrySuccess() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(500))
+        server.enqueue(MockResponse().setResponseCode(200))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.sendWithRetry(1)
+
+        Assert.assertTrue(response.isOk)
+        Assert.assertEquals(2, server.requestCount)
+    }
+
+    @Test
+    fun testSendWithRetryFailure() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(500))
+        server.enqueue(MockResponse().setResponseCode(400))
+        server.enqueue(MockResponse().setResponseCode(404))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.sendWithRetry(2)
+
+        Assert.assertFalse(response.isOk)
+        Assert.assertEquals(3, server.requestCount)
+    }
+
+    @Test
+    fun testSendWithRetryFailureNoRetries() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(500))
+        server.enqueue(MockResponse().setResponseCode(400))
+        server.enqueue(MockResponse().setResponseCode(404))
+        val baseUrl = server.url("/collect/json/")
+        val request = TestRequest(baseUrl.toString())
+
+        val response = request.sendWithRetry()
+
+        Assert.assertFalse(response.isOk)
+        Assert.assertEquals(1, server.requestCount)
+    }
+
+    @Test
+    fun testSendPostData() {
+        Lytics.configuration = LyticsConfiguration("API_KEY")
+        val server = MockWebServer()
+        server.enqueue(MockResponse().setResponseCode(200))
+        val baseUrl = server.url("/collect/json/")
+
+        val request = TestRequest(baseUrl.toString(), RequestMethod.POST)
+        val response = request.send()
+
+        Assert.assertTrue(response.isOk)
+        Assert.assertEquals(1, server.requestCount)
+
+        val recordedRequest = server.takeRequest()
+        val requestBody = BufferedReader(InputStreamReader(recordedRequest.body.inputStream())).use { it.readText() }
+        Assert.assertEquals("POST", recordedRequest.method)
+        Assert.assertEquals("POST THIS", requestBody)
     }
 }
